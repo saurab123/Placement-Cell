9ec039cc5db53c9e29176ae72882d5138a9720eb
saurab123 <69749154+saurab123@users.noreply.github.com>
2023-08-31 14:57:33 +0530

    Add files via upload

---
+++ models/student.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    batch: {
+      type: String,
+      required: true,
+    },
+    college: {
+      type: String,
+      required: true,
+    },
+    dsaScore: {
+      type: Number,
+      required: true,
+    },
+    webdScore: {
+      type: Number,
+      required: true,
+    },
+    reactScore: {
+      type: Number,
+      required: true,
+    },
+    placed: {
+      type: String,
+      enum: ["Placed", "Not placed"],
+      required: true,
+    },
+    interviews: [
+      {
+        company: {
+          type: String,
+          required: true,
+        },
+        date: {
+          type: String,
+          required: true,
+        },
+        result: {
+          type: String,
+          enum: ["pass", "on hold", "fail", "didn't attempt"].map((status) =>
+            status.toLowerCase()
+          ),
+          default: "on hold",
+          required: false,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = new mongoose.model("Student", studentSchema);
